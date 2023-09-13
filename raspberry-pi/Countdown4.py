875970ca2c2eba256f0c31a1d928b493837a1a69
pweder69 <pweder07@charlottesvilleschools.org>
2023-09-13 14:09:02 -0400

    finished

--- raspberry-pi/Countdown4.py
+++ raspberry-pi/Countdown4.py
@@ -5,8 +5,10 @@ import pwmio # type: ignore
 from adafruit_motor import servo # type: ignore
 import math
 
-pwm_servo    = pwmio.PWMOut(board.GP21, duty_cycle=2 ** 15, frequency=50)
-pwm_servo.angle = 0
+pwm   = pwmio.PWMOut(board.GP21, duty_cycle=2 ** 15, frequency=50)
+servo1 = servo.Servo(pwm, min_pulse=500, max_pulse=2500)
+
+servo1.angle = 0
 
 RedLED = digitalio.DigitalInOut(board.GP19)
 BlueLED = digitalio.DigitalInOut(board.GP13)
@@ -22,18 +24,23 @@ for x in [RedLED, BlueLED]:
     
 
 while True:
-    if not button.value():
+    if not button.value:
         
-        for x in range(7):
+        
+        for x in range(7): # Traditional countdown loop that doesnt use servo
             RedLED.value = True
-            time.sleep(.25)
+            time.sleep(.5)
             RedLED.value = False
-            time.sleep(.25)
-        
-        for x in range(180):
-            time.sleep(3/180)
-            servo.angle = x
+            time.sleep(.5)
+    
+        for x in range(1,181): # counts through 180 degrees of the servo
+            time.sleep(3/180) # sleep for 3/180 of a second every loop to make it take 3 seconds
             
-            if x % 60 == 0:
-                BlueLED.value = not BlueLED.value
+            servo1.angle = x # sets the servo angle to aproach 180 degrees at the end of the loop
+
+            if x % 30 == 0: # flip every 30 degrees which is every second 
+                print("Blink")
+                RedLED.value = not RedLED.value
                 
+        BlueLED = True
+    
